599c5dd63125937d39c976cb5018e1e6cfecc11d
EmilSantos <Santos.N.Emil@gmail.com>
2022-05-16 14:31:37 -0700

    Implemented List Encoder and started with data persistence.
    Made Vehicle object use the encoder property so that we can update the plist for data persistence. Still in progress.

---
+++ CarMaintenanceApp/CarMaintenanceApp/Data Model/Vehicle.swift
@@ -0,0 +1,13 @@
+//
+//  Vehicle.swift
+//  CarMaintenanceApp
+//
+//  Created by emil on 5/8/22.
+//
+
+import Foundation
+
+class Vehicle: Encodable {
+    var title: String = ""
+    var done: Bool = false
+}
--- CarMaintenanceApp/CarMaintenanceApp/VehicleListViewController.swift
+++ CarMaintenanceApp/CarMaintenanceApp/VehicleListViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class VehicleListViewController: UITableViewController {
 
-    var itemArray = ["Tesla Model Y","Honda Accord Hybrid","Honda Helix Couch",]
+    var itemArray = [Vehicle]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newVehicle = Vehicle()
+        newVehicle.title = "Tesla Model Y"
+        itemArray.append(newVehicle)
+        
+        let newVehicle2 = Vehicle()
+        newVehicle2.title = "Honda CBR300"
+        itemArray.append(newVehicle2)
+        
+        let newVehicle3 = Vehicle()
+        newVehicle3.title = "Go Kart 300"
+        itemArray.append(newVehicle3)
+        
+//        if let items = defaults.array(forKey: "PersistedVehicleListArray") as? [Vehicle] {
+//            itemArray = items
+//        }
         
         // Do any additional setup after loading the view.
     }
@@ -22,21 +39,27 @@ class VehicleListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "VehicleItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -45,8 +68,13 @@ class VehicleListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Vehicle", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
-            self.tableView.reloadData()
+            
+            let newVehicle = Vehicle()
+            newVehicle.title = textField.text!
+            
+            self.itemArray.append(newVehicle)
+            
+            self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -58,6 +86,19 @@ class VehicleListViewController: UITableViewController {
         
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
     }
     
 }
